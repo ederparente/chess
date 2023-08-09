c90efe7b1fe48f003b597058ff9cfa1f165afb2a
Eder Parente <eder.o.parente@gmail.com>
2023-08-09 10:58:35 -0300

    Chess layer and printing the board

--- chess/src/boardgame/Board.java
+++ chess/src/boardgame/Board.java
@@ -27,7 +27,12 @@ public class Board {
 		this.column = column;
 	}
 	
-	
-	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 }
