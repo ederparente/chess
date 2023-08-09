8da426dfb6ffc88a52e931d0f4d307666601c3c4
Eder Parente <eder.o.parente@gmail.com>
2023-08-09 10:08:55 -0300

    Starting to implement Board and Piece

--- chess/src/application/Program.java
+++ chess/src/application/Program.java
@@ -1,15 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		
-		Position pos = new Position(3,5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
 	}
 
 }
