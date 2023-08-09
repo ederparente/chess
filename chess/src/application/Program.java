6cd1dc632c10b06c0763031ce55a9de77fe0964d
Eder Parente <eder.o.parente@gmail.com>
2023-08-09 08:03:17 -0300

    class Position

--- chess/src/application/Program.java
+++ chess/src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 	}
 
 }
